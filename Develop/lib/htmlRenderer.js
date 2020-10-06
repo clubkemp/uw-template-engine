d548656e7af374e5dfbc02c8314b377dfef5db7d
Jonny K <Kempj2.jk@gmail.com>
2020-10-05 18:24:25 -0700

    adding in some todos and commenting out some gameplans

---
+++ Develop/lib/htmlRenderer.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const fs = require("fs");
+//TODO: will need to require in employee
+//setup the director to the html templates
+const templatesDir = path.resolve(__dirname, "../templates");
+
+//function that takes in employees
+const render = employees => {
+  //setup an empty array for the html to live in
+  const html = [];
+
+  //setup 3 differant push methods, but only if they pass the filter test
+  html.push(...employees
+    //filter uses the getRole method from employee
+    .filter(employee => employee.getRole() === "Manager")
+    //if it passes the filter, map over the filter and run the correect render function
+    .map(manager => renderManager(manager))
+  );
+  html.push(...employees
+    .filter(employee => employee.getRole() === "Engineer")
+    .map(engineer => renderEngineer(engineer))
+  );
+  html.push(...employees
+    .filter(employee => employee.getRole() === "Intern")
+    .map(intern => renderIntern(intern))
+  );
+
+  return renderMain(html.join(""));
+
+};
+//each function below goes through the boilerplate html and replaces things with the replacePlaceholder function
+const renderManager = manager => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
+  template = replacePlaceholders(template, "name", manager.getName());
+  template = replacePlaceholders(template, "role", manager.getRole());
+  template = replacePlaceholders(template, "email", manager.getEmail());
+  template = replacePlaceholders(template, "id", manager.getId());
+  template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
+  return template;
+};
+
+const renderEngineer = engineer => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "engineer.html"), "utf8");
+  template = replacePlaceholders(template, "name", engineer.getName());
+  template = replacePlaceholders(template, "role", engineer.getRole());
+  template = replacePlaceholders(template, "email", engineer.getEmail());
+  template = replacePlaceholders(template, "id", engineer.getId());
+  template = replacePlaceholders(template, "github", engineer.getGithub());
+  return template;
+};
+
+const renderIntern = intern => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "intern.html"), "utf8");
+  template = replacePlaceholders(template, "name", intern.getName());
+  template = replacePlaceholders(template, "role", intern.getRole());
+  template = replacePlaceholders(template, "email", intern.getEmail());
+  template = replacePlaceholders(template, "id", intern.getId());
+  template = replacePlaceholders(template, "school", intern.getSchool());
+  return template;
+};
+
+//return the main html once all the work is done
+const renderMain = html => {
+  const template = fs.readFileSync(path.resolve(templatesDir, "main.html"), "utf8");
+  return replacePlaceholders(template, "team", html);
+};
+//uses regex to create a replacement pattern
+const replacePlaceholders = (template, placeholder, value) => {
+  const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+  return template.replace(pattern, value);
+};
+
+module.exports = render;
