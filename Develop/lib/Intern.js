8895d32fc45b32acb749205879a8d0c97d3c181e
Jonny K <Kempj2.jk@gmail.com>
2020-10-06 18:20:43 -0700

    intern commented out

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,20 +1,24 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+//require in employee to extend
 const template = require("./Employee")
 
 class Intern extends template {
+    //construct the class
     constructor(name, id, email, school){
-       super(name, id, email)
+       //use super based on Employee
+        super(name, id, email)
         this.name = name,
         this.id = id
         this.email = email
         this.school = school;
     }
+    //override the getROle method from Employee
     getRole() {
         return "Intern"
     }
+    //add get school for renderer html
     getSchool() {
         return this.school;
     }
 }
-
+//export for use in app.js
 module.exports = Intern
